06be40d113d9f6c8089aa0682781fe2cc1a75659
tiagocunha77 <a37617@esas.pt>
2021-03-10 13:02:21 +0000

    autenticação

--- src/main/java/pt/tiago/catdesenhos/WebSecurityConfig.java
+++ src/main/java/pt/tiago/catdesenhos/WebSecurityConfig.java
@@ -1,14 +1,37 @@
 package pt.tiago.catdesenhos;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	UserDetailsService userDetailsService;
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable();
+		http.authorizeRequests().antMatchers("/").hasRole("ADMIN")
+		.anyRequest().authenticated()
+		.and().formLogin()
+		.and().csrf().disable();
 	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService);
+	}
+
+	@Bean
+	public PasswordEncoder getPasswordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
+	}
+
 }
